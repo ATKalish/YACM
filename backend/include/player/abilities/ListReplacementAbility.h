9b87ca5500ee3c4cb0ee7916308f5c1d1f936012
Gerrit Roessler <roessg2@rpi.edu>
2026-09-21 15:45:01 -0400

    added builder

--- backend/include/player/abilities/ListReplacementAbility.h
+++ backend/include/player/abilities/ListReplacementAbility.h
@@ -1,10 +1,35 @@
 #pragma once
 #include "Ability.h"
 #include <variant>
+
 class ListReplacementAbility : public Ability {
 private:
 	vector<string> hooks;
 	variant<string, int> count;
 	variant<vector<Ability*>/*, Spell*/> selectionList;
 	string targetAbility; //should this be a reference? dangerous with various containers
+
+public:
+	class ListReplacementBuilder : public Builder {
+		vector<string> _hooks;
+		variant<string, int> _count;
+		variant<vector<Ability*>/*, Spell*/> _selectionList;
+		string _targetAbility;
+
+
+		ListReplacementBuilder& hooks(const vector<string>& hooks) {this->_hooks = hooks; return *this;}
+		ListReplacementBuilder& count(const variant<string, int>& count) {this->_count = count; return *this;}
+		ListReplacementBuilder& selectionList(const variant<vector<Ability*>/*, *Spells*/>& selection_list) {this->_selectionList = selection_list; return *this;}
+		ListReplacementBuilder& targetAbility(const string& target) {this->_targetAbility = target; return *this;}
+		ListReplacementAbility build() {return ListReplacementAbility(this);}
+
+		friend class ListReplacementAbility;
+	};
+private:
+	ListReplacementAbility(ListReplacementBuilder* builder) : Ability(builder) {
+		this->hooks = builder->_hooks;
+		this->count = builder->_count;
+		this->selectionList = builder->_selectionList;
+		this->targetAbility = builder->_targetAbility;
+	}
 };
