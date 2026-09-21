eb85bd6da87e3e2c956989996a864b149f892958
Gerrit Roessler <roessg2@rpi.edu>
2026-09-21 10:50:34 -0400

    now using std::variant

--- backend/include/player/abilities/ListSelectionAbility.h
+++ backend/include/player/abilities/ListSelectionAbility.h
@@ -1,23 +1,16 @@
 #pragma once
 #include "Ability.h"
+#include <variant>
 
 class ListSelectionAbility : public Ability {
 private:
 	vector<string> hooks;
 	bool listIsInternal; //not 100% sure what the intention for this is
-	// TODO: convert to a std::variant
-	bool isString; //which union field to use
-	union count_{ //either it is a string id, or a set value
-		string id;
-		int value;
-	} count;
+	//bool isString; <-- implicit in count
+	variant<string, int> count;
 
-	// TODO: convert to a std::variant
-	bool forSpells;
-	union selection_list_ {
-		vector<Ability*>;
-		//vector<Spell>
-	} selection_list;
+	//bool forSpells; <-- this is implicit to selection_list
+	variant<vector<Ability*> /*, vector<Spell>*/> selection_list;
 
 public:
 	class ListBuilder : public Builder {
