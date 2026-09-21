b47e597065c67aba0717c439f57cb23ce5fef5de
Gerrit Roessler <roessg2@rpi.edu>
2026-09-21 11:33:47 -0400

    housekeeping

--- backend/include/player/abilities/ListSelectionAbility.h
+++ backend/include/player/abilities/ListSelectionAbility.h
@@ -6,17 +6,12 @@ class ListSelectionAbility : public Ability {
 private:
 	vector<string> hooks;
 	bool listIsInternal; //not 100% sure what the intention for this is
-	//bool isString; <-- implicit in count
 	variant<string, int> count;
-
-	//bool forSpells; <-- this is implicit to selection_list
 	variant<vector<Ability*> /*, vector<Spell>*/> selection_list;
 
 public:
 	class ListBuilder : public Builder {
-		string _name;
 
-		ListBuilder& name(const string& name) {this->_name = name; return *this;}
 		ListSelectionAbility build() {return ListSelectionAbility(this);}
 	};
 
