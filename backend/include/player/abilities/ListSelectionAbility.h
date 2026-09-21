e6492c97b55cdd57b28a1069caf7459d1a5eb6ba
Gerrit Roessler <roessg2@rpi.edu>
2026-09-21 11:34:21 -0400

    populated listbuilder fields

--- backend/include/player/abilities/ListSelectionAbility.h
+++ backend/include/player/abilities/ListSelectionAbility.h
@@ -11,7 +11,17 @@ private:
 
 public:
 	class ListBuilder : public Builder {
+		vector<string> _hooks;
+		bool _listIsInternal;
+		variant<string, int> _count;
+		variant<vector<Ability*>/*, *Spells*/> _selection_list;
 
+
+	public:
+		ListBuilder& hooks(const vector<string>& hooks) {this->_hooks = hooks; return *this;}
+		ListBuilder& isInternal(bool isInternal) {this->_listIsInternal = isInternal; return *this;}
+		ListBuilder& count(const variant<string, int>& _count) {this->_count = _count; return *this;}
+		ListBuilder& selection_list(const variant<vector<Ability*>/*, *Spells*/>& _selection_list) {this->_selection_list = _selection_list; return *this;}
 		ListSelectionAbility build() {return ListSelectionAbility(this);}
 	};
 
