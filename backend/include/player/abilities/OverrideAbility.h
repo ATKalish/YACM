1515740fd485a1bbd5d3595905b9865b85a091e8
Gerrit Roessler <roessg2@rpi.edu>
2026-09-22 15:29:06 -0400

    defined Builder for override class

--- backend/include/player/abilities/OverrideAbility.h
+++ backend/include/player/abilities/OverrideAbility.h
@@ -7,6 +7,25 @@ private:
 	Ability* newAbility;
 
 public:
+	class OverrideBuilder : public Builder {
+		Ability* _target;
+		Ability* _newAbility;
+
+	public:
+		OverrideBuilder& target(Ability* target) {this->_target = target; return *this;}
+		OverrideBuilder& newAbility(Ability* newAbility) {this->_newAbility = newAbility; return *this;}
+		OverrideAbility build() {return OverrideAbility(this);}
+
+		friend class OverrideAbility;
+	};
+private:
+	OverrideAbility(OverrideBuilder* builder) : Ability(builder) {
+		this->target = builder->_target;
+		this->newAbility = builder->_newAbility;
+	}
+
+	~OverrideAbility();
+
 public:
 	const Ability* getTarget() const {return this->target;}
 	const Ability* getNewAbility() const {return this->target;}
