149d79cd693c321d6d117cb8398be0aef3c16043
Gerrit Roessler <roessg2@rpi.edu>
2026-09-22 17:19:48 -0400

    defined item class

--- backend/include/player/Item.h
+++ backend/include/player/Item.h
@@ -1,24 +1,37 @@
 #pragma once
-#include <string>
-#include <vector>
+#include "abilities/Ability.h"
 
 using namespace std;
 
 class Item{
 private:
-    string name;
-    string description;
-    vector<string> abilities;
-
+	string name;
+	string description;
+	vector<Ability*> abilities;
 public:
-    Item() = default;
-    Item(const string& name, const string& description, const vector<string>& abilities){
-        this->name = name;
-        this->description = description;
-        this->abilities = abilities;
-    }
+	class Builder {
+	private:
+		string _name;
+		string _description;
+		vector<Ability*> _abilities;
+	public:
+		Builder() = default;
+		Builder& name(const string& name){this->_name = name; return *this;}
+		Builder& description(const string& description){this->_description = description; return *this;}
+		Builder& abilities(const vector<Ability*>& abilities) {this->_abilities = abilities; return *this;}
+		Item build() {return Item(this);}
 
-    const string& getName(){return this->name;}
-    const string& getDescription(){return this->description;}
-    const vector<string>* getAbilities(){return this->abilities;}
-}
+		friend class Item;
+	};
+
+	Item(Builder* builder) {
+		this->name = builder->_name;
+		this->description = builder->_description;
+		this->abilities = builder->_abilities;
+	}
+
+	~Item();
+	const string& getName() const {return this->name;}
+	const string& getDescription() const {return this->description;}
+	const vector<Ability*>& getAbilities() const {return this->abilities;}
+};
