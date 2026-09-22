6f659b4a2fb2318c2272367f0f533bc4c0c82d2b
Gerrit Roessler <roessg2@rpi.edu>
2026-09-22 17:19:27 -0400

    this should be public

--- backend/include/player/abilities/Ability.h
+++ backend/include/player/abilities/Ability.h
@@ -57,9 +57,9 @@ public:
         vector<string> _tags;
         vector<Ability*> _subAbilities;
         string _origin;
-        Builder() = default;
 
 	public:
+        Builder() = default;
         Builder& name(const string& name){this->_name = name; return *this;}
         Builder& text(const string& text){this->_text = text; return *this;}
         Builder& prereq(const string& prereq){this->_prereq = prereq; return *this;}
